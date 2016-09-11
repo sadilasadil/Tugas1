c3de9d364cbe331926895f5758da468f61143d2f
Salsadila <salsaoctavia2@gmail.com>
2016-09-11 13:31:27 +0700

    merubah interface membuat apa yg ditulis di edittext nama di tampilkan di textbox hasil dengan button daftar

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2027/tugas01/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2027/tugas01/MainActivity.java
@@ -2,12 +2,32 @@ package id.sch.smktelkom_mlg.tugas01.xirpl2027.tugas01;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    Button bDaftar;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editText);
+        bDaftar = (Button) findViewById(R.id.button);
+        tvHasil = (TextView) findViewById(R.id.textView4);
+
+
+        bDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = etNama.getText().toString();
+                tvHasil.setText("Selamat!" + nama + "\nAnda Telah Berhasil Mendaftar");
+            }
+        });
     }
 }
